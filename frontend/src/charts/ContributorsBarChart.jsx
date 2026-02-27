bda07c3f72745a4b45bcd6bf6d36b00c16300f97
sachdeva-aarushi <aarushisachdeva6@gmail.com>
2026-02-27 22:01:40 +0530

    Add contributor pie chart visualization

--- frontend/src/charts/ContributorsBarChart.jsx
+++ frontend/src/charts/ContributorsBarChart.jsx
@@ -1,4 +1,5 @@
 import { Bar } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
 
 export default function ContributorsBarChart({ data }) {
     return (
@@ -10,6 +11,24 @@ export default function ContributorsBarChart({ data }) {
                         label: "Commits",
                         data: data.map(c => c.contributions),
                         backgroundColor: "rgba(11, 119, 161, 0.6)"
+                    }
+                ]
+            }}
+        />
+
+    );
+}
+export default function ContributorsPieChart({ data }) {
+    return (
+        <Pie
+            data={{
+                labels: data.map(c => c.login),
+                datasets: [
+                    {
+                        data: data.map(c => c.percentage),
+                        backgroundColor: data.map(() =>
+                            `hsl(${Math.random() * 360}, 70%, 60%)`
+                        )
                     }
                 ]
             }}
